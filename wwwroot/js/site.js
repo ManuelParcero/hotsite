4f673804e45ff63a398aa1f34786abfa51edc8fd
ManuelParcero <esmeraldinoar@gmail.com>
2023-04-08 17:09:41 -0300

    inclusao_form_val

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+function validarFormulario() {
+    let x = document.forms["formulario"]["fname"].value;
+    let y = document.forms["formulario"]["femail"].value;
+    if (x == "") {
+      alert("O campo nome precisa ser preenchido");
+      return false;
+    }
+    if (y == "") {
+        alert("O campo email precisa ser preenchido");
+        return false;
+      }
+  } 
